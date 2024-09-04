5c2217f2f6078553e32016173e83ea1637797216
havishat <sundariha@gmail.com>
2024-09-04 15:39:23 -0700

    Update decrease_and_conquer.js

--- decrease_and_conquer.js
+++ decrease_and_conquer.js
@@ -47,3 +47,54 @@ function numberOfOnes(arr) {
   }
   return arr.length - start;
 }
+
+
+/*
+ * ## Closest Value
+ *
+ * *Given a sorted bit array of integers, and a target value, find the number in the array that is closest to the target.*
+ *
+ * **Parameters**
+ * Input: arr {Array of Integers}
+ * Input: target {Integer}
+ * Output: {Integer}
+ *
+ * **Constraints**
+ * If there are two numbers tied for the closest value, return the lowest value.
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `[1, 2, 3, 5, 5, 7, 9, 10, 11], 6 --> 5`
+ * `[1, 2, 3], 8 --> 3`
+ * `[1, 10, 22, 59, 67, 72, 100], 70 --> 72`
+ */
+
+
+function closestValue(arr, target) {
+    let closet = Infinity; 
+    let start = 0; 
+    let end = arr.length - 1;
+    let value = null;
+    
+    while(start <= end ){
+        let mid = Math.floor((start+end)/2);
+        difference = Math.abs(target - arr[mid]);
+        
+        if(difference < closet){
+            closet = difference;
+            value = arr[mid];
+        }
+        
+        if(arr[mid] < target){
+            start = mid + 1;
+        } else {
+            end = mid -1 
+        }
+    }
+    return value
+}
+
+console.log(closestValue([1, 10, 22, 59, 67, 72, 100], 70))
+
