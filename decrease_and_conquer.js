53dd9be910b9c86a897e53a203bd5a19c6df6328
havishat <sundariha@gmail.com>
2024-09-05 16:06:50 -0700

    Update decrease_and_conquer.js

--- decrease_and_conquer.js
+++ decrease_and_conquer.js
@@ -162,3 +162,67 @@ function greaterValues(arr, target) {
 }
 
 console.log(greaterValues([1, 10, 22, 59, 67, 72, 100], 13))
+
+
+
+/*
+ * ## Sorted and Rotated Array [Extra Credit]
+ * *Given an array with distinct values that is sorted and rotated, find out if a target value exists in the array.*
+ *
+ * A sorted array is rotated by taking an unknown amount of values from the beginning and placing it at the end.
+ *
+ * `[3, 4, 5, 1, 2]` is rotated left by 2.
+ * `[99, 14, 25, 78, 93]` is rotated to the right by 1.
+ *
+ * **Parameters**
+ * Input: arr {Array}
+ * Output: {Boolean}
+ *
+ * **Constraints**
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `[35, 46, 79, 102, 1, 14, 29, 31], 46 --> true`
+ * `[35, 46, 79, 102, 1, 14, 29, 31], 47 --> false`
+ * `[7, 8, 9, 10, 1, 2, 3, 4, 5, 6], 9 --> true`
+ */
+
+
+function rotatedArraySearch(nums, target) {
+    let start = 0; 
+    let end = nums.length - 1;
+    while(start < end && nums[start] > nums[end]){
+        let mid = parseInt((start + end)/2)
+        if(nums[start]  < nums[end]){
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+    
+    if(target >= nums[0]){
+        return binarySearch(nums, 0, end, target);
+    } else {
+        return binarySearch(nums, end + 1, nums.length - 1, target)
+    }
+}
+
+function binarySearch(nums, start, end, target) {
+    while(start <= end) {
+        let mid = parseInt((start + end)/2);
+        if(nums[mid] === target){
+            return true
+        }
+        
+        if(nums[mid] < target){
+            start = mid + 1;
+        } else {
+            end = mid - 1
+        }
+    }
+    return false
+}
+
+console.log(rotatedArraySearch([7, 8, 9, 10, 1, 2, 3, 4, 5, 6], 9))
+console.log(rotatedArraySearch([35, 46, 79, 102, 1, 14, 29, 31], 47))
