28920225bfa799d7e25e01990818862950ef321d
havishat <sundariha@gmail.com>
2024-09-05 16:13:50 -0700

    Update decrease_and_conquer.js

--- decrease_and_conquer.js
+++ decrease_and_conquer.js
@@ -226,3 +226,42 @@ function binarySearch(nums, start, end, target) {
 
 console.log(rotatedArraySearch([7, 8, 9, 10, 1, 2, 3, 4, 5, 6], 9))
 console.log(rotatedArraySearch([35, 46, 79, 102, 1, 14, 29, 31], 47))
+
+
+/*
+ * ## Multiplication Using Russian Peasant [Extra Credit]
+ *
+ * *Given two positive integers, return its product using Russian Peasant method of multiplication*
+ *
+ * Read up on how to apply the Russian Peasant method [here](https://en.wikipedia.org/wiki/Ancient_Egyptian_multiplication). It is also referred to as the Egyptian multiplication.
+ *
+ * **Parameters**
+ * Input: a {Integer}
+ * Input: b {Integer}
+ * Output: {Integer}
+ *
+ * **Constraints**
+ * Assume a <= b, and the value of a is N.
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `487, 734 --> 357458`
+ * `846, 908--> 768168`
+ */
+
+function  multiplicationRussianPeasant(a, b) {
+  let finalSum = 0;
+  while (b >= 1) {
+    if (b % 2 != 0) {
+      finalSum += a;
+    }
+    if (b === 1) {
+      break;
+    }
+    a *= 2;
+    b = parseInt(b / 2);
+  }
+  return finalSum;
+}
